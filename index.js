ae46b1ce13bfe0afe95d935e753472ac9b9dbbdc
vanishs <vanishs@hotmail.com>
2016-12-05 11:18:33 +0800

    change to umd module

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-define(["require", "exports"], function (require, exports) {
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
     "use strict";
     var umdwebsocket = (function () {
         function umdwebsocket(addr, proto, reconnectMaxTimeS, _onMessage, _onOpen, _onClose, _onError) {
